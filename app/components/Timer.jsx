20d57ebf8aa6372637e0e291981689651f09ac5f
jtackettFS <jtackett@fullsail.com>
2018-01-17 15:05:22 -0500

    Committing to work on at home

---
+++ app/components/Timer.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Clock = require('Clock');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <p>Timer.jsx</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
