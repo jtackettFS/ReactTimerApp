71406f5559e82f68dfab0405c5ea75a3d295c393
jtackettFS <jtackett@fullsail.com>
2018-01-18 09:54:37 -0500

    forgot to add countdown.test.jsx

---
+++ app/tests/components/countdownform.test.jsx
@@ -0,0 +1,37 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var CountdownForm = require('CountdownForm');
+
+describe('CountdownForm', () => {
+   it('should exist', () => {
+       expect(CountdownForm).toExist();
+       });
+
+   it('should call onSetCountdown if valid seconds are entered', () => {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+      countdownForm.refs.seconds.value = '109';
+      // Pulling out first DOM node using jQuery
+      TestUtils.Simulate.submit($el.find('form')[0]);
+
+      expect(spy).toHaveBeenCalledWith(109);
+   });
+
+    it('should not call onSetCountdown if invalid seconds are entered', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = 'asdf';
+        // Pulling out first DOM node using jQuery
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalled();
+    });
+});
